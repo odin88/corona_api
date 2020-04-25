662c7da06d9bccededacf9cc32f7bb31de8bb855
Hassan Abu Bakar <odin88@gmail.com>
2020-04-26 01:41:49 +0800

    Add project files.

---
+++ WpfApp1/CountriesDataPOCO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1
+{
+    public class CountriesDataPOCO
+    {
+
+        public class Rootobject
+        {
+            public bool success { get; set; }
+            public Result[] result { get; set; }
+        }
+
+        public class Result
+        {
+            public string country { get; set; }
+            public string totalCases { get; set; }
+            public string newCases { get; set; }
+            public string totalDeaths { get; set; }
+            public string newDeaths { get; set; }
+            public string totalRecovered { get; set; }
+            public string activeCases { get; set; }
+        }
+
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using RestSharp;
+using RestSharp.Serializers.NewtonsoftJson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new RestClient("https://api.collectapi.com/corona/countriesData");
+            client.UseNewtonsoftJson();
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("authorization", "apikey 0KpVbgSHTnb6GtZ3bhoRtH:2qUGmVyPckfEmaW1VKhYKv");
+            request.AddHeader("content-type", "application/json");
+
+            var results = client.Execute<CountriesDataPOCO.Rootobject>(request);
+            var records = results.Data;
+
+            Country.Text = records.result.First().country;
+            Confirmed.Text = records.result.First().activeCases;
+            Death.Text = records.result.First().totalDeaths;
+            Recovered.Text = records.result.First().totalRecovered;
+
+        }
+    }
+}
